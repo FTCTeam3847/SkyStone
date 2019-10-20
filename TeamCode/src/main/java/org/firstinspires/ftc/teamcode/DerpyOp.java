0eeec4b9903deaa8230b76c2aa39984c6c9c43d9
Puma Robotics <p2proboticsftc@gmail.com>
2019-10-20 15:45:35 -0600

    The "A" button is now a full toggle and not a coin flip for each press.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DerpyOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DerpyOp.java
@@ -6,6 +6,8 @@ import com.qualcomm.hardware.bosch.BNO055IMU;
 public class DerpyOp extends BaseOp {
     public boolean slowmode = false;
     public boolean testMode = false;
+    public boolean prevState = true;
+    public long lastTime = System.currentTimeMillis();
     public BNO055IMU imu;
     public ChasisObject drive;
 
@@ -16,26 +18,32 @@ public class DerpyOp extends BaseOp {
         drive = new ChasisObject(imu);
     }
 
+
     @Override
     public void loop() {
-            if (testMode)
-                drive.tempCalculate(gamepad1.left_stick_x, gamepad1.left_stick_y, gamepad1.right_stick_x);
-            else
-                drive.calculate(gamepad1.left_stick_x, gamepad1.left_stick_y, gamepad1.right_stick_x);
-            if (slowmode)
-                move4(drive.getLeftFor() / 2, drive.getLeftBack() / 2, drive.getRightFor() / 2, drive.getRightBack() / 2);
-            else
-                move4(drive.getLeftFor(), drive.getLeftBack(), drive.getRightFor(), drive.getRightBack());
+        long currentTime = System.currentTimeMillis();
+    boolean buttonAPressed = gamepad1.a;
+        if (testMode)
+            drive.tempCalculate(gamepad1.left_stick_x, gamepad1.left_stick_y, gamepad1.right_stick_x);
+        else
+            drive.calculate(gamepad1.left_stick_x, gamepad1.left_stick_y, gamepad1.right_stick_x);
+        if (slowmode)
+            move4(drive.getLeftFor() / 2, drive.getLeftBack() / 2, drive.getRightFor() / 2, drive.getRightBack() / 2);
+        else
+            move4(drive.getLeftFor(), drive.getLeftBack(), drive.getRightFor(), drive.getRightBack());
         if (gamepad1.b) {
             slowmode = !slowmode;
         }
-        if (gamepad1.a) {
+        if (prevState == false && buttonAPressed == true) {
             testMode = !testMode;
         }
         telemetry.addData("Target Angle", drive.getTargetAngle());
         telemetry.addData("Current Angle", drive.getCurrentAngle());
         telemetry.addData("Slow Mode", slowmode);
         telemetry.addData("Test Mode", testMode);
+        telemetry.addData("loopMS:",currentTime - lastTime);
         telemetry.update();
+        prevState = buttonAPressed;
+        lastTime = currentTime;
     }
 }

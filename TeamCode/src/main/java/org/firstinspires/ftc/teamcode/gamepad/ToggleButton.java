eb611468f7ebf2142a26b414912ab8cc5362e1b5
Brian Dupras <bdupras@twitter.com>
2019-10-20 21:38:35 -0600

    adds ToggleButtonTest as an assignment

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/gamepad/ToggleButton.java
@@ -0,0 +1,58 @@
+package org.firstinspires.ftc.teamcode.gamepad;
+
+import java.util.function.Supplier;
+
+/**
+ * Keeps track of a toggle value in response to button presses on a
+ * gamepad. It's expected that an OpMode will call {@link #get()}
+ * once per `loop()` to obtain the current value of the toggle.
+ *
+ * The tricky part is that loop() runs many times per second, and so the
+ * button state is checked many times per second. It's difficult for a user
+ * to tap and release a button in the time it takes for one loop() to
+ * complete. Because of this, a naive implementation that flips the toggle
+ * value whenever the button state is pressed actually causes the toggle to
+ * flip many times per physical button-press. The result to the end user is
+ * a seemingly random toggle value after tapping the button.
+ *
+ * This timeline depicts 27 iterations through an OpMode's `loop()` calling
+ * `get()`, and so the button is checked 27 times and is wither unpressed
+ * (U) or pressed (P). The user has only pressed the button 5 times, and so
+ * the toggle should only flip 5 times.
+ *
+ * Assuming the starting toggle value is false (F), then the final toggle
+ * value after 5 button presses should be true (T).
+ *
+ * Iterations through loop() - each column is one loop()
+ *        Presses:        ↓       ↓         ↓         ↓       ↓
+ *   Button State:  F F F T T F F T T T F F T T F F F T T F F T T T F F F ...
+ *   Toggle Value:  F F F T T T T F F F F F T T T T T F F F F T T T T T T ...
+ */
+public class ToggleButton {
+    private Supplier<Boolean> readButtonState;
+
+    /**
+     * Advanced Question: Why do we use the Supplier type here? Why not
+     * pass a Gamepad type instead?
+     */
+    public ToggleButton(Supplier<Boolean> readButtonState) {
+        this.readButtonState = readButtonState;
+    }
+
+    /**
+     * TODO: fix `get()` by implementing real logic.
+     *
+     * Things to think about:
+     *   How do you know when to flip the toggle? What are the conditions?
+     *   What stateful variable do you need to track?
+     *   Where does a Java class store its stateful variables?
+     */
+    public Boolean get() {
+        // Read the state of the button from the gamepad.
+        boolean buttonStateNow = readButtonState.get();
+
+        // We must return a value to satisfy the compiler so the tests can execute.
+        // When you write the code, remember to modify this return statement.
+        return false;
+    }
+}
---
+++ TeamCode/src/test/java/org/firstinspires/ftc/teamcode/gamepad/ToggleButtonTest.java
@@ -0,0 +1,118 @@
+package org.firstinspires.ftc.teamcode.gamepad;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+/**
+ * Assignment 1: Test-driven development - or TDD - is a coding
+ * technique where you first write a test that fails, and then
+ * you write or change the application's code to make the test
+ * pass.
+ *
+ * Here I've written code to test a class called ToggleButton.
+ * The ToggleButton class has just enough code to allow the test
+ * to compile and run, but it lacks real functionality so the
+ * test fails.
+ *
+ * Your challenge:
+ *   1. Run the test below and inspect the test output.
+ *
+ *   2. Open the ToggleButton class and read its comments to
+ *      learn what it's supposed to do.
+ *
+ *   3. Modify ToggleButton and re-run the test until you make
+ *      the test pass.
+ *
+ *   4. You may not change any code in this test. No cheating!
+ */
+class ToggleButtonTest {
+
+    class FakeGamePad {
+        boolean a = false;
+    }
+
+    /**
+     * A good test should demonstrated how the tested code is
+     * used. In this case, the code we're testing is ToggleButton,
+     * which would be used in an iterative OpMode. So here we have
+     * a fake OpMode that looks a bit like the real thing.
+     *
+     * DerpyOp uses gamepad1.a to set a `testMode` variable to decide
+     * which drive-train code to use.
+     */
+    class FakeDerpyOp {
+        FakeGamePad gamepad1 = new FakeGamePad();
+
+        // This is really how you would use ToggleButton in an OpMode.
+        // Note the use of the lambda function `() -> gamepad1.a` to
+        // read from the controller. Can you explain why a lambda here
+        // is better than passing gamepad1 or gamepad1.a to the
+        // ToggleButton constructor?
+        ToggleButton toggleButtonA = new ToggleButton(() -> gamepad1.a);
+        boolean testMode = false;
+
+        public void loop() {
+            testMode = toggleButtonA.get();
+        }
+    }
+
+    /**
+     * In this test, we simulate an iterative OpMode. We control the
+     * gamepad as though we are the user. We invoke the OpMode's loop()
+     * many times just as a real OpMode's loop() is called iteratively.
+     */
+    @Test
+    void get() {
+        final FakeDerpyOp derpyOp = new FakeDerpyOp();
+
+        // user hasn't pushed the A button yet - testMode should
+        // remain false through multiple loop() iterations.
+        derpyOp.loop();
+        assertFalse(derpyOp.testMode);
+        derpyOp.loop();
+        assertFalse(derpyOp.testMode);
+
+        // pushing the button for the first time - testMode should flip to true
+        derpyOp.gamepad1.a = true;
+        derpyOp.loop();
+        assertTrue(derpyOp.testMode); // <---- the test should fail here - that is, until you write ToggleButton's logic. Off you go. Best of luck. You got this.
+        derpyOp.loop();
+        assertTrue(derpyOp.testMode);
+
+        // release the button - testMode should remain true
+        derpyOp.gamepad1.a = false;
+        derpyOp.loop();
+        assertTrue(derpyOp.testMode);
+        derpyOp.loop();
+        assertTrue(derpyOp.testMode);
+
+        // pushing the button a second time - testMode should flip to false
+        derpyOp.gamepad1.a = true;
+        derpyOp.loop();
+        assertFalse(derpyOp.testMode);
+        derpyOp.loop();
+        assertFalse(derpyOp.testMode);
+
+        // release the button again - testMode should remain set to false
+        derpyOp.gamepad1.a = false;
+        derpyOp.loop();
+        assertFalse(derpyOp.testMode);
+        derpyOp.loop();
+        assertFalse(derpyOp.testMode);
+
+        // pushing the button for the third time - toggle should flip again to true
+        derpyOp.gamepad1.a = true;
+        derpyOp.loop();
+        assertTrue(derpyOp.testMode);
+        derpyOp.loop();
+        assertTrue(derpyOp.testMode);
+
+        // release the button again - toggle should remain be set to true
+        derpyOp.gamepad1.a = false;
+        derpyOp.loop();
+        assertTrue(derpyOp.testMode);
+        derpyOp.loop();
+        assertTrue(derpyOp.testMode);
+    }
+}

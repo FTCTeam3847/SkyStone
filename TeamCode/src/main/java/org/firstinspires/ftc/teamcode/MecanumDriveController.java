3cf5e765337e2d7b79c2f17394b8ce00792845e7
CharlieRayP2P <cqray01@bvsd.org>
2019-11-02 10:29:23 -0600

    Recoded mecanum drive to hopefully fix the unit circle problems and the turning driving problems, may be overfitted to the testing data.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumDriveController.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumDriveController.java
@@ -46,17 +46,43 @@ public class MecanumDriveController {
             double rad = Math.sqrt(pow(left_x, 2) + pow(left_y, 2));
             double theta = Math.atan2(left_y, left_x);
             double magnitude = pow(rad, sensitivity);
+            double rightFor = (Math.sin(theta - 45) * magnitude + correction)/Math.abs((Math.sin(theta - 45)));
+            double rightBack = (Math.sin(theta + 45) * magnitude + correction)/Math.abs((Math.sin(theta - 45)));
+            double leftFor = (Math.sin(theta + 45) * magnitude - correction)/Math.abs((Math.sin(theta - 45)));
+            double leftBack = (Math.sin(theta - 45) * magnitude - correction)/Math.abs((Math.sin(theta - 45)));
+
+
+            double maxOffset = maxAbs(rightFor, rightBack, leftFor, leftBack, 1.0);
+            rightFor = rightFor/maxOffset;
+            rightBack = rightBack/maxOffset;
+            leftFor = leftFor/maxOffset;
+            leftBack = leftBack/maxOffset;
+            double[] wheels = new double[]{rightFor, rightBack, leftFor, leftBack};
+            for (int i = 0; i < wheels.length; i++) {
+                if (Math.abs(wheels[i]) < 0.09) {
+                    wheels[i] = 0.0;
+                }
+            }
+            rightFor = wheels[0];
+            rightBack = wheels[1];
+            leftFor = wheels[2];
+            leftBack = wheels[3];
+
 
             drivePower = new DrivePower(
-                    (Math.sin(theta - 45) * magnitude + correction)/Math.abs((Math.sin(theta - 45) * magnitude + correction)),
-                    (Math.sin(theta + 45) * magnitude + correction)/Math.abs((Math.sin(theta - 45) * magnitude + correction)),
-                    (Math.sin(theta + 45) * magnitude - correction)/Math.abs((Math.sin(theta - 45) * magnitude + correction)),
-                    (Math.sin(theta - 45) * magnitude - correction)/Math.abs((Math.sin(theta - 45) * magnitude + correction)));
+                    rightFor, rightBack, leftFor, leftBack);
         }
         last_x = right_x;
         return drivePower;
 
     }
+    private static double maxAbs(double... wheels) {
+        double max = 0.0;
+        for (int i = 0; i < wheels.length; i++) {
+            max = Math.max(Math.abs(wheels[i]), max);
+        }
 
+        return max;
+    }
 
 }

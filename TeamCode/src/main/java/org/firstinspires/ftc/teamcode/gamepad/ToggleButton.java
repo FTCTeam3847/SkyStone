54fb17c8d6e77b33a61552172d8cb4c78b80b008
Brian Dupras <bdupras@twitter.com>
2019-12-15 18:36:07 -0700

    fixed comments in ToggleButton

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/gamepad/ToggleButton.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/gamepad/ToggleButton.java
@@ -7,33 +7,34 @@ import java.util.function.Supplier;
 /**
  * Keeps track of a toggle value in response to button presses on a
  * gamepad. It's expected that an OpMode will call {@link #getCurrent()}
- * once per `getLast()` to obtain the current value of the toggle.
+ * once per `getCurrent()` to obtain the current value of the toggle.
  * <p>
- * The tricky part is that getLast() runs many times per second, and so the
- * button state is checked many times per second. It's difficult for a user
- * to tap and release a button in the time it takes for one getLast() to
- * complete. Because of this, a naive implementation that flips the toggle
- * value whenever the button state is pressed actually causes the toggle to
- * flip many times per physical button-press. The result to the end user is
- * a seemingly random toggle value after tapping the button.
+ * The tricky part is that getCurrent() runs many times per second, and so
+ * the button state is checked many times per second. It's difficult for a
+ * user to tap and release a button in the time it takes for one
+ * getCurrent() to complete. Because of this, a naive implementation that
+ * flips the toggle value whenever the button state is pressed actually
+ * causes the toggle to flip many times per physical button-press. The
+ * result to the end user is a seemingly random toggle value after tapping
+ * the button.
  * <p>
- * This timeline depicts 27 iterations through an OpMode's `getLast()` calling
- * `getLast()`, and so the button is checked 27 times and is wither unpressed
- * (F) or pressed (T). The user has only pressed the button 5 times, and so
- * the toggle should only flip 5 times.
+ * This timeline depicts 27 iterations through an OpMode's `loop()` calling
+ * `getCurrent()`, and so the button is checked 27 times and is either
+ * unpressed (F) or pressed (T). The user has only pressed the button 5
+ * times, and so the toggle should only flip 5 times.
  * <p>
  * Assuming the starting toggle value is false (F), then the final toggle
  * value after 5 button presses should be true (T).
  * <p>
- * Iterations through getLast() - each column is one getLast()
- * Presses:        ↓       ↓         ↓         ↓       ↓
+ * Iterations through getCurrent() - each column is one getCurrent()
+ * Presses:             ↓       ↓         ↓         ↓       ↓
  * Button State:  F F F T T F F T T T F F T T F F F T T F F T T T F F F ...
  * Toggle Value:  F F F T T T T F F F F F T T T T T F F F F T T T T T T ...
  */
 public class ToggleButton implements Sensor<Boolean> {
     private Supplier<Boolean> readButtonState;
-    boolean buttonIsToggled = false;
-    boolean previousState = false;
+    private boolean previousButtonState = false;
+    private boolean toggleValue = false;
 
     /**
      * Advanced Question: Why do we use the Supplier type here? Why not
@@ -44,8 +45,6 @@ public class ToggleButton implements Sensor<Boolean> {
     }
 
     /**
-     * TODO: fix `getLast()` by implementing real logic.
-     * <p>
      * Things to think about:
      * How do you know when to flip the toggle? What are the conditions?
      * What stateful variables do you need to track?
@@ -55,14 +54,12 @@ public class ToggleButton implements Sensor<Boolean> {
         // Read the state of the button from the gamepad.
         boolean buttonStateNow = readButtonState.get();
 
-        if (buttonStateNow && !previousState) {
-            buttonIsToggled = !buttonIsToggled;
+        if (buttonStateNow && !previousButtonState) {
+            toggleValue = !toggleValue;
         }
 
-        previousState = buttonStateNow;
+        previousButtonState = buttonStateNow;
 
-        return buttonIsToggled;
-        // We must return a value to satisfy the compiler so the tests can execute.
-        // When you write the code, remember to modify this return statement.
+        return toggleValue;
     }
 }

a9a947e443669376782610f6f4a65cab53ad01f3
Puma Robotics <p2proboticsftc@gmail.com>
2020-01-05 11:05:56 -0700

    offest camera by -6in

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SkyStoneLocalizer.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SkyStoneLocalizer.java
@@ -217,7 +217,7 @@ public class SkyStoneLocalizer implements Localizer<FieldPosition> {
 
         // Next, translate the camera lens to where it is on the robot.
         // In this example, it is centered (left to right), but forward of the middle of the robot, and above ground level.
-        final float CAMERA_FORWARD_DISPLACEMENT = 0f * mmPerInch;   // eg: Camera is 4 Inches in front of robot center
+        final float CAMERA_FORWARD_DISPLACEMENT = -6f * mmPerInch;   // eg: Camera is 4 Inches in front of robot center
         final float CAMERA_VERTICAL_DISPLACEMENT = 0.0f * mmPerInch;   // eg: Camera is 8 Inches above ground
         final float CAMERA_LEFT_DISPLACEMENT = 0;     // eg: Camera is ON the robot's center line
 

df62732a3acf2a1c5f7e92a508616892a577cda7
Puma Robotics <p2proboticsftc@gmail.com>
2020-01-05 19:45:57 -0700

    adds RTP controller and setPosition to BlockLifter

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Trinkets/BlockLifter.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Trinkets/BlockLifter.java
@@ -1,10 +1,11 @@
 package org.firstinspires.ftc.teamcode.Trinkets;
 
+import org.firstinspires.ftc.teamcode.controller.RunToPositionController;
+
 import java.util.Locale;
 import java.util.function.Consumer;
 
 import static com.qualcomm.robotcore.util.Range.clip;
-import static com.qualcomm.robotcore.util.Range.scale;
 import static java.lang.Math.max;
 import static java.lang.Math.min;
 import static java.lang.String.format;
@@ -21,6 +22,8 @@ public class BlockLifter {
     private double power = 0.0d;
     private static final double MAX_POSITION = 2200.0d;
     private final PositionIntegrator position = new PositionIntegrator(System::nanoTime, 0.0d, MAX_POSITION);
+    private RunToPositionController runToPositionController = new RunToPositionController(this::getPower, this::getPosition);
+
 
     public BlockLifter(Consumer<Double> leftPower,
                        Consumer<Double> rightPower
@@ -30,31 +33,59 @@ public class BlockLifter {
 
     }
 
-    public void setPower(double power) {
-        if (power > 0 && position.getPosition() >= MAX_POSITION) {
+    public void loop() {
+        if (isRunningPastLimits()) {
+            stop();
+        } else if (runToPositionController.isBusy()) {
+            runAtPower(runToPositionController.getControl());
+        }
+    }
+
+    public void stop() {
+        runToPositionController.stop();
+        runAtPower(0.0d);
+    }
+
+    private boolean isRunningPastLimits() {
+        return power > 0 && position.getPosition() >= MAX_POSITION;
+    }
+
+    private void runAtPower(double power) {
+        if (isRunningPastLimits()) {
             power = 0;
         } else {
             power = clip(power, -1.0d, 1.0d);
         }
+
         position.setVelocity(power);
         leftPower.accept(power);
         rightPower.accept(power);
         this.power = power;
     }
 
+    // [-1.0..1.0] negative is down, positive is up
+    public void setPower(double pwr) {
+        // if the user starts controlling the power, then cancel the rtp controller
+        runToPositionController.stop();
+        runAtPower(pwr);
+    }
+
     public double getPower() {
         return this.power;
     }
 
-    //Percentage
+    // [0.0..1.0] 0.0 is down, 1.0 is up
     public double getPosition() {
         return limit(0.0d, position.getPosition() / MAX_POSITION, 1.0d);
+    }
+
+    public void setPosition(double targetPosition) {
+        runToPositionController.setTarget(targetPosition);
     }
 
     private static double limit(double min, double n, double max) {
         return min(max(min, n), max);
     }
-
 
     @Override
     public String toString() {

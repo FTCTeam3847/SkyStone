f1b61d4f1504066ca1cb987c0fbfafb7afb1bbb4
pumarobotics <p2probotics@gmail.com>
2020-01-20 14:31:41 -0700

    doesnt reset script on gamepad stop

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BaseOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BaseOp.java
@@ -1,14 +1,10 @@
 package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
-import com.qualcomm.robotcore.hardware.CRServo;
-import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.hardware.Servo;
 
 import org.firstinspires.ftc.teamcode.action.SequentialAction;
 import org.firstinspires.ftc.teamcode.action.SkystoneScripts;
 import org.firstinspires.ftc.teamcode.bot.SkystoneBot;
-import org.firstinspires.ftc.teamcode.drive.DrivePower;
 
 
 public abstract class BaseOp extends OpMode {
@@ -43,7 +39,6 @@ public abstract class BaseOp extends OpMode {
     public void loop() {
         if (gamepad1.x || gamepad2.x) {
             script.stop();
-            script = scripts.emptyScript();
         } else {
             script.loop();
         }

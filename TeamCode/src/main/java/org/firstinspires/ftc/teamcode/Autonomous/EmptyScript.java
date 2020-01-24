c7f45959cb6e4e4b3345f3df9f590330d2b569ed
Puma Robotics <p2proboticsftc@gmail.com>
2020-01-23 18:29:47 -0700

    make EmptyScript like other op modes

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/EmptyScript.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/EmptyScript.java
@@ -4,11 +4,18 @@ import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 
 import org.firstinspires.ftc.teamcode.BaseOp;
 
+import static org.firstinspires.ftc.teamcode.GameConstants.FACING_REAR_WALL;
+import static org.firstinspires.ftc.teamcode.controller.FieldPosition.fieldPosition;
+import static org.firstinspires.ftc.teamcode.polar.CartesianCoord.xy;
+
 @Autonomous
 public class EmptyScript extends BaseOp {
     @Override
     public void start() {
         super.start();
-        script = scripts.emptyScript().start();
+        script = scripts.emptyScript()
+                .run(() -> bot.getLocalizer().calibrate(fieldPosition(xy(-39, -54), FACING_REAR_WALL)))
+                .run(() -> bot.getMecanumDrive().stop())
+                .start();
     }
 }
